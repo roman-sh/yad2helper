95926a346f87f7342b3fc47d7724ee44687bb333
Roman Shmigelsky <shmigelsky@gmail.com>
2019-12-24 14:27:38 +0200

    Pass moved to process.env

--- mailer.js
+++ mailer.js
@@ -4,7 +4,7 @@ var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'raichy@gmail.com',
-        pass: 'par0ll1218'
+        pass: process.env.RAICHY_PASS
     }
 })
 
