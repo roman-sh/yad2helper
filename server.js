3a1e6a77ef33a9dd0977d4bef8f2b6b4e8859b01
Roman Shmigelsky <shmigelsky@gmail.com>
2019-12-24 15:12:10 +0200

    trobleshooting mailer

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const {config} = require('./config')
 
 console.log('...STARTING...')
 console.log('process.env.RAICHY_PASS:', process.env.RAICHY_PASS)
+sendMail()
 
 schedule.scheduleJob('0 * * * *', () => {
     request(config, callback)
